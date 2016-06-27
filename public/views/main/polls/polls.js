54fffadfb53554fcce9e11badd98a56699b3dee2
goPaddle <testgopaddle@gmail.com>
2016-06-27 18:46:50 +0530

    Update polls.js

--- public/views/main/polls/polls.js
+++ public/views/main/polls/polls.js
@@ -4,4 +4,9 @@ angular.module('pollApp')
 		$scope.open_poll = function (id) {
 			$state.go('main.show_poll', {id: id});
 		};
+		$scope.deletePoll = function(id){
+          Poll.delete({ id: id }, function(response) {
+            $scope.polls = Poll.query();
+          });
+  };
 	});
